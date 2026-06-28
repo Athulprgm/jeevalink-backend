530d86620fca0bdbf31e414477eda9a2ccf0961a
Athul Krishna <athulkrishna.cpd@gmail.com>
2026-06-28 15:33:07 +0530

    fix(db): change logo column from string to longText for base64 support

--- database/migrations/2026_06_28_000001_create_partners_table.php
+++ database/migrations/2026_06_28_000001_create_partners_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('partners', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('logo');
+            $table->longText('logo');
             $table->string('social_media_type');
             $table->string('social_media_link');
             $table->timestamps();
