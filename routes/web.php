8d0de47d3d1da902058551740cabe698acd15475
Athul Krishna <athulkrishna.cpd@gmail.com>
2026-06-29 14:57:01 +0530

    Remove test email route

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,3 @@ Route::get('/health', function () {
     ]);
 });
 
-Route::get('/test-email', function () {
-    return view('emails.volunteer_welcome', [
-        'name' => 'John Doe',
-        'email' => 'athulkrishnacpd@gmail.com',
-        'password' => 'SecurePass123!',
-        'loginUrl' => 'http://localhost:5173/login'
-    ]);
-});
