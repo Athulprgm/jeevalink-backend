0730f998c8898167c65453d865ac677506b80e4b
Athul Krishna <athulkrishna.cpd@gmail.com>
2026-06-29 17:02:31 +0530

    Fix: remove ShouldQueue to ensure immediate email delivery on live server

--- app/Mail/VolunteerWelcomeMail.php
+++ app/Mail/VolunteerWelcomeMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class VolunteerWelcomeMail extends Mailable implements ShouldQueue
+class VolunteerWelcomeMail extends Mailable
 {
     use Queueable, SerializesModels;
 
